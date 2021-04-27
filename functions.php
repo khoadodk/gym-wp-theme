c6063b20af7c6cb3671c3bbdaf46e15814e27a4b
Khoa Do <khoado.dk@gmail.com>
2021-04-26 23:25:08 -0400

    Added about us and classes page

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php 
+//import files
+require get_template_directory() . '/inc/queries.php';
+
 
 /*
-*==============================
+==============================
         MENUS
-*==============================
+==============================
 */
 function gym_menus() {
     register_nav_menus(
@@ -15,9 +18,9 @@ function gym_menus() {
 add_action( 'init', 'gym_menus');
 
 /*
-*==============================
-        CSS and JS
-*==============================
+==============================
+        ENQUEUE CSS and JS
+==============================
 */
 function gym_scripts(){
     // Normalize CSS
@@ -40,3 +43,39 @@ function gym_scripts(){
     wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'gym_scripts');
+
+/*
+==============================
+        ENABLE FEATURE IMAGE
+==============================
+*/
+function gym_setup() {
+    // Register new images with Regenerate Thumbnails Plugin
+    add_image_size('square', 350, 350, true);
+    add_image_size('portrait', 350, 724, true);
+    add_image_size('box', 400, 375, true);
+    add_image_size('medoumSize', 700, 400, true);
+    add_image_size('blog', 966, 644, true);
+
+    // Add featured image
+    add_theme_support('post-thumbnails');
+}
+add_action( 'after_setup_theme', 'gym_setup' ); //When theme is activated and ready
+
+/*
+==============================
+        WIDGET ZONE
+==============================
+*/
+function gym_widgets(){
+    register_sidebar(array(
+        'name' => 'Sidebar',
+        'id'=>'sidebar',
+        'before_widget' => '<div class="widget">',
+        'after_widget'=>'</div>',
+        'before_title'=>'<h3>',
+        'after_title'=>'</h3>',
+    ));
+}
+
+add_action('widgets_init', 'gym_widgets');
---
+++ page-classes.php
@@ -0,0 +1,7 @@
+<?php get_header();?>
+    <main class="container page section classes-index">
+        <?php
+            gym_classes_list();
+        ?>
+    </main>
+<?php get_footer(); ?>
